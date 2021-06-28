5d31fb279f457cfe025f47fa1831a605da72a9a8
alexellevu <newnapster83@gmail.com>
2021-06-28 08:52:20 +0200

    settato oggetto con variabili

--- index.php
+++ index.php
@@ -14,14 +14,17 @@ class Movie
     public $director;
     public $year;
     public $language;
+    public $img;
+
     function __construct(
-       string $title, string $desc , string $director, int $year ,  string $language)
+       string $title, string $desc , string $director, int $year ,  string $language, string $img ='')
     {
         $this ->title= $title;
         $this ->desc = $desc;
         $this ->director = $director;
         $this ->year =$year;
         $this->language = $language;
+        $this->img = $img;
 
 
     }
@@ -32,10 +35,14 @@ class Movie
     
 }
 
+$movies =[
+    new Movie('Titanic', 'Lorem ipsum dolor, sit amet consectetur adipisicing.','James Cameroon',1998,'english','https://movieplayer.net-cdn.it/t/images/2009/09/29/la-locandina-di-titanic-7522_jpg_400x0_crop_q85.jpg'),
+    new Movie('Sin City', 'Lorem ipsum dolor, sit amet consectetur adipisicing.','Frank Miller',2005,'english','https://pad.mymovies.it/filmclub/2005/05/015/locandina.jpg'),
+    new Movie('La Vita è Bella', 'Lorem ipsum dolor, sit amet consectetur adipisicing.','Roberto Benigni',1998,'italian','http://www.cgentertainment.it/img/film/3690/LOC1_3690.jpg'),
+    new Movie('Trainspotting', 'Lorem ipsum dolor, sit amet consectetur adipisicing.','Danny Boyle',1996,'english','https://pad.mymovies.it/filmclub/2006/08/133/locandina.jpg')
 
+];
 
-$Titanic = new Movie('Titanic','Lorem ipsum dolor sit amet consectetur adipisicing elit.','James Cameron',1998,'english');
-var_dump($Titanic);
 
 ?>
 
@@ -45,9 +52,24 @@ var_dump($Titanic);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
     <title>Document</title>
 </head>
 <body>
+        <div class="movies">
+           <?php foreach($movies as $movie): ?>
+                <div class="movie">
+                <img src="<?php echo $movie->img ?>" alt="">
+                <h2> <?php echo $movie->title ?> </h2>
+                <p><?php echo $movie->desc ?></p>
+                <p>Directed by: <?php echo $movie->director ?></p>
+                <p>Year: <?php echo $movie->year?></p>
+                <p>language: <?php echo $movie->language?></p>
 
+                
+                </div>
+
+            <?php endforeach; ?>  
+        </div>
 </body>
 </html>
